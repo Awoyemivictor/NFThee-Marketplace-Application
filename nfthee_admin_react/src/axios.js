e62b5abed9ed95a394845302e118e182d43141cb
Chetan Deshpande <chetan.deshpande@protonmail.com>
2023-02-28 12:00:41 +0530

    updated code

--- nfthee_admin_react/src/axios.js
+++ nfthee_admin_react/src/axios.js
@@ -1,12 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:"http://192.168.1.48:8004",
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-    }
+  // baseURL:"http://192.168.1.48:8004",
+  baseURL: process.env.REACT_APP_ADMIN_RENDER_BASE_URL,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
+  },
 });
 
 export default instance;
--- nfthee_react/src/axios.js
+++ nfthee_react/src/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   // local
-  baseURL: process.env.REACT_APP_BASE_URL+'/',
-  // baseURL: process.env.REACT_APP_RENDER_BASE_URL + '/',
+  // baseURL: process.env.REACT_APP_BASE_URL+'/',
+  baseURL: process.env.REACT_APP_RENDER_BASE_URL + '/',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
