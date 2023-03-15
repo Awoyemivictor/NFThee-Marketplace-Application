816446e7e84550d9ac30207493fa021d6d5e6312
Chetan Deshpande <chetan.deshpande@protonmail.com>
2023-03-15 18:41:21 +0530

    added airdrop token  coontract andAPI

--- nfthee_solidity/scripts/deploy.js
+++ nfthee_solidity/scripts/deploy.js
@@ -1,54 +1,48 @@
+const { network } = require('hardhat');
 const hre = require('hardhat');
 
+const PaymentTokenAddress = {
+  bsctestnet: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
+  bsc: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+  matic: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', // WMATIC
+  mumbai: '',
+  eth: '0x418D75f65a02b3D53B2418FB8E1fe493759c7605',
+  harmony: '0xb1f6E61E1e113625593a22fa6aa94F8052bc39E0',
+  harmonyDev:''
+};
+
+const provider = hre.ethers.provider;
 async function main() {
-  const Market = await hre.ethers.getContractFactory('Market');
-  const Offer = await hre.ethers.getContractFactory('Offer');
-  const Auction = await hre.ethers.getContractFactory('Auction');
-  const Sale = await hre.ethers.getContractFactory('Sale');
+  const gasPrice = await provider.getGasPrice();
+  // console.log(
+  //   'Gas price on',
+  //   network,
+  //   hre.ethers.utils.formatEther(gasPrice, 'gwei'),
+  //   'gwei'
+  // );
+  // console.log( network)
+  //   if (network === 'deployment') {
+  const ERC721 = await hre.ethers.getContractFactory('TestERC721');
+  let erc721 = await ERC721.deploy();
+  await erc721.deployed();
+  console.log(
+    `TestERC721 deployed at ${erc721.address} in network: ${network}.`
+  );
+  const ERC20 = await hre.ethers.getContractFactory('TestERC20');
+  const erc20 = await ERC20.deploy();
+  await erc20.deployed();
 
-  const market = await Market.deploy();
-  const offer = await Offer.deploy();
-  const auction = await Auction.deploy();
-  const sale = await Sale.deploy();
+  console.log(`TestERC20 deployed at ${erc20.address} in network: ${network}.`);
 
-  await market.deployed();
+  const Marketplace = await hre.ethers.getContractFactory('Marketplace');
 
-  console.log(`Marketplace Contract Deployed at ${market.address}`);
-  console.log(`Offer Contract Deployed at ${offer.address}`);
-  console.log(`Auction   Contract Deployed at ${auction.address}`);
-  console.log(`Sale Contract Deployed at ${sale.address}`);
+  const marketplace = await Marketplace.deploy(erc20.address);
+  await marketplace.deployed();
 
-  //* ----------------- Auto Verification Function -------------
-
-  await sleep(1000);
-
-  await hre.run('verify:verify', {
-    address: market.address,
-  });
-
-  // for (let i = 0; i <= 3; i++) {
-  //   console.log(i);
-  //   await sleep(1000);
-  //   let contractAddress;
-  //   if (i === 0) {
-  //     contractAddress = address.market;
-  //   } else if (i === 1) {
-  //     contractAddress = address.offer;
-  //   } else if (i === 2) {
-  //     contractAddress = address.auction;
-  //   } else {
-  //     contractAddress = address.sale;
+  console.log(
+    `Marketplace deployed at ${marketplace.address} in network: ${network}.`
+  );
   //   }
-
-  //   await hre.run('verify:verify', {
-  //     address: contractAddress,
-  //   });
-
-  // }
-}
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 main().catch((error) => {
